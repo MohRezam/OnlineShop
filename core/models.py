6df29347561cf370ab10089403d3e10cc631a84b
MohRezam <mkalhor81126@gmail.com>
2024-01-24 20:48:39 +0330

    Added core/models.py for BaseModel & add BaseModel in it

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models import Manager, QuerySet
 
-# Create your models here.
+class AppQuerySet(QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+
+        
+class AppManager(Manager):
+    def get_queryset(self):
+        return QuerySet(self.model, using=self._db).exclude(is_deleted=True)
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+        
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(auto_now=True, editable=False)
+    
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+                
+    objects = AppManager()
