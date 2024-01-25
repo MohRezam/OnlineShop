6383fbd16bdb085f764b69d14f01d6a7c09ff1be
MohRezam <mkalhor81126@gmail.com>
2024-01-26 03:27:44 +0330

    Registered Category, Product, Comments & Discount models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product, Comment, Discount
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Comment)
+admin.site.register(Discount)
