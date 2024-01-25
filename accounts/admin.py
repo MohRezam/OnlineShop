c67a4e6d3e77f9c33b7ce3743dc075f21ef34695
MohRezam <mkalhor81126@gmail.com>
2024-01-26 03:21:14 +0330

    Registered User & Address models, unregistered Group model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Address
+from django.contrib.auth.models import Group
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.unregister(Group)
