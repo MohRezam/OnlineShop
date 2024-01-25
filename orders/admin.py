eaba68b6b5afc85ef4b551392a3575df7afabb01
MohRezam <mkalhor81126@gmail.com>
2024-01-26 03:24:57 +0330

    Registered Order, OrderItem, Coupon & Transacion models in orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Coupon, Transaction
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Coupon)
+admin.site.register(Transaction)
