0b82e5908effa2ed51613127a93f8c39ce6068f1
MohRezam <mkalhor81126@gmail.com>
2024-01-26 03:29:03 +0330

    Added Comment model & static fields instead of bool field & set related_name in products/models.py

--- products/models.py
+++ products/models.py
@@ -5,37 +5,55 @@ from core.utils import category_image_path, product_image_path
 # Create your models here.
 
 class Category(BaseModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     is_sub = models.BooleanField(default=False)
     image = models.ImageField(upload_to=category_image_path)
     
     # Foreign Keys
-    parent_category = models.ForeignKey("self", on_delete=models.PROTECT)
-    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True)
+    parent_category = models.ForeignKey("self", on_delete=models.PROTECT, related_name="categories")
+    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True, related_name="categories")
     
     
 class Product(BaseModel):
+    PRODUCT_ACTIVE_CHOICES = (
+        (True, "Active"),
+        (False, "Not Active"),
+    )
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
-    price = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    slug = models.SlugField()
     inventory_quantity = models.PositiveIntegerField()
+    is_active = models.CharField(max_length=25, choices=PRODUCT_ACTIVE_CHOICES, default=True)
     image = models.ImageField(upload_to=product_image_path)
     
-    # Foreign Keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT) # this relation is between staff and Product not customer.
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True)
-
     
+    # Foreign Keys
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="products") # this relation is between staff and Product not customer.
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products")
+    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True, related_name="products") # had blank and null True
+
+class Comment(BaseModel):
+    text = models.TextField()
+    
+    # Foreign Keys
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="comments")
+    products = models.ForeignKey("Product", on_delete=models.PROTECT, related_name="comments")
+    
+
 class Discount(BaseModel):
+    DISCOUNT_TYPE = (
+        ("percentage", "Percentage"),
+        ("decimal", "Decimal"),
+    )
     type = models.CharField(max_length=255)
     value = models.DecimalField(max_digits=10, decimal_places=2)
     max_value = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=False)
     
     # Foreign Keys
-    user = models.ManyToManyField(User) # this relation is between staff and Discount not customer.
+    user = models.ManyToManyField(User, related_name="discounts") # this relation is between staff and Discount not customer.
     
     
     
