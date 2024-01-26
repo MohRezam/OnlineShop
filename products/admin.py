4e15069f0f6ba22d93723779c5107b444fb3ea3f
MohRezam <mkalhor81126@gmail.com>
2024-01-27 00:03:14 +0330

    Registered ProductFeaures and ProductFeatureValues in products/admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Comment, Discount
+from .models import Category, Product, Comment, Discount, ProductFeature, ProductFeatureValue
 
 # Register your models here.
 
@@ -7,3 +7,5 @@ admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Discount)
+admin.site.register(ProductFeature)
+admin.site.register(ProductFeatureValue)
