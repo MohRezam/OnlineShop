e18000d1469e2603f5d788a1de411aa5e9671306
MohRezam <mkalhor81126@gmail.com>
2024-01-26 23:59:29 +0330

    Added custom authentication backend in the settings

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.User"
+
+AUTHENTICATION_BACKENDS = [
+    "accounts.authenticate.PhoneBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
