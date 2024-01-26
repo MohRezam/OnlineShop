893c758167a7e94b025d1e600e8c6c3318da8be4
MohRezam <mkalhor81126@gmail.com>
2024-01-27 00:03:53 +0330

    Added __str__ and verbose_name_plural for Meta class

--- products/models.py
+++ products/models.py
@@ -10,10 +10,15 @@ class Category(BaseModel):
     image = models.ImageField(upload_to=category_image_path)
     
     # Foreign Keys
-    parent_category = models.ForeignKey("self", on_delete=models.PROTECT, related_name="categories")
-    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True, related_name="categories")
+    parent_category = models.ForeignKey("self", on_delete=models.PROTECT)
+    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True)
     
+    def __str__(self) -> str:
+        return f"{self.name}"
     
+    class Meta:
+        verbose_name_plural = 'categories'
+            
 class Product(BaseModel):
     PRODUCT_ACTIVE_CHOICES = (
         (True, "Active"),
@@ -24,24 +29,58 @@ class Product(BaseModel):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     slug = models.SlugField()
+    features = models.ManyToManyField("ProductFeature", through='ProductFeatureValue')
     inventory_quantity = models.PositiveIntegerField()
     is_active = models.CharField(max_length=25, choices=PRODUCT_ACTIVE_CHOICES, default=True)
     image = models.ImageField(upload_to=product_image_path)
     
     
     # Foreign Keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="products") # this relation is between staff and Product not customer.
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products")
-    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True, related_name="products") # had blank and null True
+    user = models.ForeignKey(User, on_delete=models.PROTECT) # this relation is between staff and Product not customer.
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    discount = models.ForeignKey("Discount", on_delete=models.PROTECT, null=True, blank=True) # had blank and null True
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+    class Meta:
+        verbose_name_plural = 'products'
+
+class ProductFeature(BaseModel):
+    name = models.CharField(max_length=255)
+    text_value = models.TextField(blank=True, null=True)
+    numeric_value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+    class Meta:
+        verbose_name_plural = 'features'
+class ProductFeatureValue(BaseModel):
+    value = models.CharField(max_length=255)
+    
+    # Foreign Keys
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    feature = models.ForeignKey(ProductFeature, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.value} for {self.feature.name}"
+    
+    class Meta:
+        verbose_name_plural = 'feature values'
 class Comment(BaseModel):
     text = models.TextField()
     
     # Foreign Keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="comments")
-    products = models.ForeignKey("Product", on_delete=models.PROTECT, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    products = models.ForeignKey("Product", on_delete=models.PROTECT)
     
+    def __str__(self) -> str:
+        return f"{self.text} by {self.user.first_name} {self.user.last_name}"
 
+    class Meta:
+        verbose_name_plural = 'comments'
+        
 class Discount(BaseModel):
     DISCOUNT_TYPE = (
         ("percentage", "Percentage"),
@@ -53,7 +92,12 @@ class Discount(BaseModel):
     is_active = models.BooleanField(default=False)
     
     # Foreign Keys
-    user = models.ManyToManyField(User, related_name="discounts") # this relation is between staff and Discount not customer.
+    user = models.ManyToManyField(User) # this relation is between staff and Discount not customer.
     
+    def __str__(self) -> str:
+        return f"{self.type}| {self.value}"
+    
+    class Meta:
+        verbose_name_plural = 'discounts'
     
     
