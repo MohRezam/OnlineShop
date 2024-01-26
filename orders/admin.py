52d578b6f4ea6c874963451920d65a6941b03c55
MohRezam <mkalhor81126@gmail.com>
2024-01-27 00:01:18 +0330

    Registered Cart & CartItem in the orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Order, OrderItem, Coupon, Transaction
+from .models import Order, OrderItem, Coupon, Transaction, Cart, CartItem
 
 # Register your models here.
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 admin.site.register(Coupon)
 admin.site.register(Transaction)
