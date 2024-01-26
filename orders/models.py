ffe1e444a84db9c9fcc0d9d37f6145f95c91d21b
MohRezam <mkalhor81126@gmail.com>
2024-01-27 00:02:28 +0330

    Added create_order_from_cart function in orders/models.py  & added __str__ and verbose_name_plural for Meta class

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,15 @@ from accounts.models import User
 from products.models import Product
 
 # Create your models here.
+def create_order_from_cart(self, cart): # this cart is an object of Cart model
+    order = self.create(user=cart.user, total_price=cart.calculate_total_price())
+
+    for cart_item in cart.cart_items.all():
+        OrderItem.objects.create(order=order, product=cart_item.product, quantity=cart_item.quantity)
+
+    cart.delete()
+
+    return order
 
 class Order(BaseModel):
     PAYMENT_CHOICES = (
@@ -11,21 +20,73 @@ class Order(BaseModel):
         (False, "Not Paid")
     )
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    is_paid = models.CharField(max_length=25, choices=PAYMENT_CHOICES)
+    is_paid = models.CharField(max_length=25, choices=PAYMENT_CHOICES, default=False) # when we create order from cart we have to set is_paid to True
+    province = models.CharField(max_length=255, blank=True, null=True) # if user dont fill this it is going to fill by user address model informations
+    city = models.CharField(max_length=255, blank=True, null=True)
+    detailed_address = models.TextField(blank=True, null=True)
+    postal_code = models.PositiveIntegerField(blank=True, null=True)
     
-    # Foreign keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="orders")
-    coupon = models.ForeignKey("Coupon", on_delete=models.PROTECT, blank=True, null=True, related_name="orders")
+    # Foreign Keys
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    coupon = models.ForeignKey("Coupon", on_delete=models.PROTECT, blank=True, null=True)
     
+    def __str__(self) -> str:
+        return f"Total: {self.total_price}, Payment: {self.is_paid}"
+    class Meta:
+        verbose_name_plural = 'orders'
     
 class OrderItem(BaseModel):
     quantity = models.IntegerField()
     
-    # Foreign keys
-    order = models.ForeignKey("Order", on_delete=models.PROTECT, related_name="order_items")
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="order_items")
+    # Foreign Keys
+    order = models.ForeignKey("Order", on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     
+    def __str__(self) -> str:
+        return f"number of {self.product.name}: {self.quantity}"
+    class Meta:
+        verbose_name_plural = 'order items'
 
+class Cart(models.Model):
+    province = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    detailed_address = models.TextField(blank=True, null=True)
+    postal_code = models.PositiveIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Foreign Keys
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    
+    def __str__(self) -> str:
+        return f"total cart: {self.calculate_total_price}"
+    
+    def calculate_total_price(self):
+        return sum(item.total_price() for item in self.cart_items.all())
+
+    class Meta:
+        verbose_name_plural = 'carts'
+
+class CartItem(models.Model):
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Foreign Keys
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    
+    def __str__(self) -> str:
+        return f"total price of {self.quantity} {self.product.name}: {self.total_price}"
+    
+    def total_price(self):
+        return self.quantity * self.product.price
+    
+    class Meta:
+        verbose_name_plural = 'cart items'
+        
 class Coupon(BaseModel):
     code = models.CharField(max_length=255, unique=True)
     percentage = models.PositiveIntegerField(default=1)
@@ -35,9 +96,14 @@ class Coupon(BaseModel):
     is_active = models.BooleanField(default=True)
     
     # Foreign Keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="coupons")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
     
+    def __str__(self) -> str:
+        return f"{self.code} with {self.percentage} percentage is active until {self.expiration_date} for {self.available_quantity} people"
     
+    class Meta:
+        verbose_name_plural = 'coupons'
+        
 class Transaction(BaseModel):
     TRANSACTION_TYPES = (
     ("accounting transactions", "accounting transactions"),
@@ -47,7 +113,12 @@ class Transaction(BaseModel):
     transaction_type = models.CharField(max_length=255, choices=TRANSACTION_TYPES) # deleted in ERD!
     
     # Foreign Keys
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="transactions")
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="transactions")
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
     
+    def __str__(self) -> str:
+        return f"transaction: {self.final_price}"
+    
+    class Meta:
+        verbose_name_plural = "transactions"
 
