c63f7e368667e110b80941f791bcad98be7eb240
MohRezam <mkalhor81126@gmail.com>
2024-02-01 02:16:51 +0330

    Remove comments!

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,4 @@
 def user_image_path(instance, filename):
-    # Generate a unique path based on user ID and current date
     user_id = instance.id if instance.id else 'temp'
     today = instance.created_at.strftime('%Y/%m/%d')
     return f'users/{user_id}/{today}/{filename}'
@@ -8,4 +7,4 @@ def category_image_path(instance, filename):
     return f'category/{instance.name}/{filename}'
 
 def product_image_path(instance, filename):
-    return f'products/{instance.category.name}/{filename}'
+    return f'products/{instance.category.name}/{instance.name}/{filename}'
