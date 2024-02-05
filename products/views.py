82ddd89a3098ca61fd702d96c88d181c486a4196
MohRezam <mkalhor81126@gmail.com>
2024-02-06 01:17:59 +0330

    Added HomeView in my products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Category, Comment, News
+from rest_framework.response import Response
+from .serializers import CategorySerializer, CommentSerializer, NewsSerializer
+from rest_framework import status
 # Create your views here.
+
+
+class HomeView(APIView):
+    def get(self, request):
+        categories = Category.objects.filter(is_sub=False)
+        comments = Comment.objects.all().order_by('-likes')[:10]
+        news = News.objects.all()
+        
+        ser_cat_data = CategorySerializer(instance=categories, many=True)
+        ser_comm_data = CommentSerializer(instance=comments, many=True)
+        ser_news_data = NewsSerializer(instance=news, many=True)
+        
+        response_data = {
+            'categories': ser_cat_data.data,
+            'comments': ser_comm_data.data,
+            'news':ser_news_data.data,
+        }
+
+        return Response(data=response_data, status=status.HTTP_200_OK)
