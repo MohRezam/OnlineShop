7d721d30680b249e0d6ce673bedb7fa8dd35dacf
MohRezam <mkalhor81126@gmail.com>
2024-02-06 17:29:18 +0330

    Added CategoryAdmin & ProductAdmin which they filter the category field in the admin panel to show them base on is_sub is True or not

--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,27 @@ from .models import Category, Product, Comment, Discount, ProductFeature, Produc
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Discount)
 admin.site.register(ProductFeature)
 admin.site.register(ProductFeatureValue)
 admin.site.register(News)
 
+
+class ProductAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = db_field.related_model.objects.filter(is_sub=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent_category":
+            kwargs["queryset"] = Category.objects.filter(is_sub=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Category, CategoryAdmin)
