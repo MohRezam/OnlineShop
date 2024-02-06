f0b82f915cd03be947f40bc4067ec10375ce532a
MohRezam <mkalhor81126@gmail.com>
2024-02-06 17:33:21 +0330

    Added url for the category, product and product details

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,10 @@ app_name="products"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("category/<slug:category_slug>/", views.CategoryView.as_view(), name="category"),
+    path("category/<slug:category_slug>/<slug:subcategory_slug>/products/", views.ProductView.as_view(), name="products"),
+    path("products/<slug:slug>/", views.ProductDetailView.as_view(), name="product_detail"),
+
 ]
+
+
