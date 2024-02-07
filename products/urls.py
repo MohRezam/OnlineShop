b6bdaaeee6df653f7bbc37d7c504456b0ae9835c
MohRezam <mkalhor81126@gmail.com>
2024-02-08 00:24:14 +0330

    Added urls for pages & for the APIs

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,15 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("category/<slug:category_slug>/", views.CategoryView.as_view(), name="category"),
     path("category/<slug:category_slug>/<slug:subcategory_slug>/products/", views.ProductView.as_view(), name="products"),
-    path("products/<slug:slug>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("products/<slug:product_slug>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("about/", views.AboutUsView.as_view(), name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
+
+    
+    path("api/", views.HomeAPIView.as_view(), name="home_api"),
+    path("api/category/<slug:category_slug>/", views.CategoryAPIView.as_view(), name="category_api"),
+    path("api/category/<slug:category_slug>/<slug:subcategory_slug>/products/", views.ProductAPIView.as_view(), name="products_api"),
+    path("api/products/<slug:slug>/", views.ProductDetailAPIView.as_view(), name="product_detail_api"),
 
 ]
 
