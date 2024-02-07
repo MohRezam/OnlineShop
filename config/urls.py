dea1bef9118a019be60a1d8de3c586fd806ded3f
MohRezam <mkalhor81126@gmail.com>
2024-02-08 00:21:59 +0330

    Added static settings at config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("products.urls", namespace="products")),
-] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
