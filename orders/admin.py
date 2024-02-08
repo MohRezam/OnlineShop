10d9166e364b2a99876b393ccd058ef58ce20bf2
MohRezam <mkalhor81126@gmail.com>
2024-02-08 04:33:01 +0330

    Registered orders models

--- orders/admin.py
+++ orders/admin.py
@@ -3,9 +3,42 @@ from .models import Order, OrderItem, Coupon, Transaction, Cart, CartItem
 
 # Register your models here.
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Coupon)
-admin.site.register(Transaction)
+# admin.site.register(Order)
+# admin.site.register(OrderItem)
+# admin.site.register(Cart)
+# admin.site.register(CartItem)
+# admin.site.register(Coupon)
+# admin.site.register(Transaction)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'is_paid', 'created_at', 'updated_at')
+    search_fields = ('id', 'user')
+    list_filter = ('is_paid', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity')
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'updated_at')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cart', 'product', 'quantity')
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'percentage', 'expiration_date', 'available_quantity')
+    search_fields = ('code',)
+    list_filter = ('expiration_date',)
+    ordering = ('-expiration_date',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'user', 'final_price','created_at')
+    search_fields = ('id', 'final_price')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
