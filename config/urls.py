b80d7fb223c18d17e0a566291775203499cda389
MohRezam <mkalhor81126@gmail.com>
2024-02-08 04:32:34 +0330

    Added accounts urls

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("products.urls", namespace="products")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
