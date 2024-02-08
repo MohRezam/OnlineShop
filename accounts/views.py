c6424881cd0b1be67ea4f561afefd2940cd0b825
MohRezam <mkalhor81126@gmail.com>
2024-02-08 04:32:06 +0330

    Added view for login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class UserLoginView(View):
+    def get(self, request):
+        return render(request, "accounts/login.html", {})
