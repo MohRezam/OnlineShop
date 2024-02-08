c85ff7e3067273a43613684019353231ef0cb04f
MohRezam <mkalhor81126@gmail.com>
2024-02-08 04:35:23 +0330

    Added serializer for all of the products models

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Comment, News, Product
+from .models import Category, Comment, News, Product, ProductFeature, ProductFeatureValue, Discount
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,20 +8,43 @@ class CategorySerializer(serializers.ModelSerializer):
         
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+        
+
+class ProductFeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductFeature
+        fields = "__all__"
+        
+
+    
+class ProductFeatureValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductFeatureValue
+        fields = "__all__"
+        
+
+        
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
     class Meta:
         model = Comment
         fields = "__all__"
-        
+
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = "__all__"
+
+
         
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
-        fields = "__all__"
-        
-        
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
         fields = "__all__"
