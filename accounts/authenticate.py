66a4713369430c06056427f9a20282dbd383aa35
MohRezam <mkalhor81126@gmail.com>
2024-02-10 00:31:08 +0330

    Set Email for Authentication instead of Phone number authentication

--- accounts/authenticate.py
+++ accounts/authenticate.py
@@ -1,9 +1,29 @@
+from typing import Any
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.http import HttpRequest
 from .models import User
 from django.contrib.auth.backends import BaseBackend
-class PhoneBackend(BaseBackend):
+# class PhoneBackend(BaseBackend):
+#     def authenticate(self, request, username=None, password=None, **kwargs):
+#         try:
+#             user = User.objects.get(phone_number=username)
+#             if user.check_password(password):
+#                 return user
+#             return None
+#         except User.DoesNotExist:
+#             return None    
+    
+    
+#     def get_user(self, user_id):
+#         try:
+#             return User.objects.get(pk=user_id)
+#         except User.DoesNotExist:
+#             return None
+
+class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(phone_number=username)
+            user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
             return None
@@ -16,4 +36,4 @@ class PhoneBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-        
+                
