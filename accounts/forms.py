5a86d6768fac550458d510feceb3751e539652f3
MohRezam <mkalhor81126@gmail.com>
2024-02-10 00:32:19 +0330

    Modified the ValidationError message

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,7 +16,7 @@ class UserCreationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd["password1"] and cd["password2"] and cd["password1"] != cd['password2']:
-            raise ValidationError("passwords dont match")
+            raise ValidationError("passwords must match")
         return cd["password2"]
    
     def save(self, commit=True):
