676dcf4b5c268df68bb9de763bad40afa4c18bf1
MohRezam <mkalhor81126@gmail.com>
2024-02-10 00:33:23 +0330

    Deleted address argument in create_user & create_superuser cause there is no address field in the User model so ...

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import BaseUserManager
-from django.db.models import Manager, QuerySet
+from django.db.models import QuerySet
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, email, first_name, last_name, password, address=None):
+    def create_user(self, phone_number, email, first_name, last_name, password):
         if not phone_number:
             raise ValueError("User must have a phone number")
         
@@ -22,20 +22,18 @@ class UserManager(BaseUserManager):
             email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
-            address=address,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, phone_number, email, first_name, last_name, password, address=None):
+    def create_superuser(self, phone_number, email, first_name, last_name, password):
         user = self.create_user(
             phone_number=phone_number,
             email=email,
             first_name=first_name,
             last_name = last_name,
             password=password,
-            address=address,
         )
         user.is_staff = True
         user.save(using=self._db)
