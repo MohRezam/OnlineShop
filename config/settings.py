5aa828f6ea0efd07b7f5ff38b501a14502acfe7c
MohRezam <mkalhor81126@gmail.com>
2024-02-14 01:48:00 +0330

    Added JWT authentication settings

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,9 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES':[
+  'rest_framework_simplejwt.authentication.JWTAuthentication', 
+],
 
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 'PAGE_SIZE':6,
