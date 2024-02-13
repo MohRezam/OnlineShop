4696ce5a8f332e96836a5d15058c63a24110fbb6
MohRezam <mkalhor81126@gmail.com>
2024-02-14 01:46:05 +0330

    Added urls for register, login & verify

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 
+
 app_name="accounts"
 
 urlpatterns = [
+    path("register/", views.UserRegisterView.as_view(), name="user_register"),
     path("login/", views.UserLoginView.as_view(), name="user_login"),
+    path("verify/", views.VerifyCodeView.as_view(), name="verify_code"),
+    
+    #API
+    path("api/register/", views.UserRegisterAPIView.as_view(), name="user_register_api"),
+    # path("api/login/", views.User)
+   
 ]
