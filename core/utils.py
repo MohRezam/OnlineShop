b90f4e91f4e4fc9b6992f813f23cd9d22cdcccbb
MohRezam <mkalhor81126@gmail.com>
2024-02-14 01:49:59 +0330

    Added kavenegar api for sending SMS

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,5 @@
+from kavenegar import *
+
 def user_image_path(instance, filename):
     user_id = instance.id if instance.id else 'temp'
     today = instance.created_at.strftime('%Y/%m/%d')
@@ -11,3 +13,34 @@ def product_image_path(instance, filename):
 
 def news_image_path(instance, filename):
     return f'news/{instance.title}/{filename}'
+
+
+# def send_otp_code(phone_number, code):
+#     try:
+#         api = KavenegarAPI("2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D")
+#         params = {
+#             'sender':'',
+#             'receptor':phone_number,
+#             'message': f"Code: {code}"
+#         }
+#         response = api.sms_send(params)
+#     except APIException as e:
+#         print(e)
+#     except HTTPException as e:
+#         print(e)
+
+from kavenegar import *
+try:
+    api = KavenegarAPI('2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D')
+    params = {
+        'receptor': '09016058919',
+        'template': '',
+        'token': '',
+        'type': 'sms',#sms vs call
+    }   
+    response = api.verify_lookup(params)
+    print(response)
+except APIException as e: 
+    print(e)
+except HTTPException as e: 
+    print(e)
