1fab684263d68e6c1415c59194f58559a68c7ae2
MohRezam <mkalhor81126@gmail.com>
2024-02-14 01:54:50 +0330

    Uncomment send_otp_code function

--- core/utils.py
+++ core/utils.py
@@ -15,32 +15,32 @@ def news_image_path(instance, filename):
     return f'news/{instance.title}/{filename}'
 
 
-# def send_otp_code(phone_number, code):
-#     try:
-#         api = KavenegarAPI("2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D")
-#         params = {
-#             'sender':'',
-#             'receptor':phone_number,
-#             'message': f"Code: {code}"
-#         }
-#         response = api.sms_send(params)
-#     except APIException as e:
-#         print(e)
-#     except HTTPException as e:
-#         print(e)
+def send_otp_code(phone_number, code):
+    try:
+        api = KavenegarAPI("2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D")
+        params = {
+            'sender':'',
+            'receptor':phone_number,
+            'message': f"Code: {code}"
+        }
+        response = api.sms_send(params)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
 
-from kavenegar import *
-try:
-    api = KavenegarAPI('2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D')
-    params = {
-        'receptor': '09016058919',
-        'template': '',
-        'token': '',
-        'type': 'sms',#sms vs call
-    }   
-    response = api.verify_lookup(params)
-    print(response)
-except APIException as e: 
-    print(e)
-except HTTPException as e: 
-    print(e)
+# from kavenegar import *
+# try:
+#     api = KavenegarAPI('2F6B33347A696134443838414A6F487372493455516749386A6548736B6C4B5467446E58782B41764734303D')
+#     params = {
+#         'receptor': '09016058919',
+#         'template': '',
+#         'token': '',
+#         'type': 'sms',#sms vs call
+#     }   
+#     response = api.verify_lookup(params)
+#     print(response)
+# except APIException as e: 
+#     print(e)
+# except HTTPException as e: 
+#     print(e)
