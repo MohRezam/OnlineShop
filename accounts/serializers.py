1d609aa8ae10f8c87c122d412700258723a96e1e
MohRezam <mkalhor81126@gmail.com>
2024-02-14 18:04:27 +0330

    Added validation for phone number, email and password

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import User, OtpCode
 from django.contrib import messages
 from django.shortcuts import redirect
+import re
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(required=True)
@@ -15,19 +16,45 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ["first_name", "last_name", "phone_number", "email", "password"]
     
     
-    # def validate_phone(self, value):
-    #     if User.objects.filter(phone_number=value):
-    #         raise serializers.ValidationError("This phone number is already in use")
-    #     return value
+    def validate_phone_number(self, value):
+        if User.objects.filter(phone_number=value):
+            raise serializers.ValidationError("This phone number is already in use")
+        elif len(value) < 11:
+            raise serializers.ValidationError("Your phone number is not correct. Must be 11 digits long")
+        return value
+        
     
     
-    # def validate_email(self, value):
-    #     if User.objects.filter(email=value).exists():  # Potential cause of recursion
-    #         raise serializers.ValidationError("This email address is already in use.")
-    #     return value
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():  
+            raise serializers.ValidationError("This email address is already in use.")
+        return value
+
+    def validate_password(self, value):
+        if len(value) < 6:
+            raise serializers.ValidationError("Password must be at least 6 characters long")
+        return value
         
         
-        
+# class UserLoginSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ["email", "password"]
+
+#     def validate_email(self, value):
+#         # Custom email validation logic
+#         if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", value):
+#             raise serializers.ValidationError("Invalid email format")
+#         return value
+
+#     def validate_password(self, value):
+#         # Add custom password validation logic here
+#         # For example, you can check if the password meets certain criteria
+#         if len(value) < 6:
+#             raise serializers.ValidationError("Password must be at least 6 characters long")
+#         return value
+
+
 class OtpCodeSerializer(serializers.ModelSerializer):
     code = serializers.CharField(required=True)
     class Meta:
