2138e4a02bc534e89e2d464ed73256f7b35a3c1e
MohRezam <mkalhor81126@gmail.com>
2024-02-14 18:05:47 +0330

    Added login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path("register/", views.UserRegisterView.as_view(), name="user_register"),
     path("login/", views.UserLoginView.as_view(), name="user_login"),
     path("verify/", views.VerifyCodeView.as_view(), name="verify_code"),
+    path("logout/", views.UserLogOutView.as_view(), name="user_logout"),
     
     #API
     path("api/register/", views.UserRegisterAPIView.as_view(), name="user_register_api"),
-    # path("api/login/", views.User)
+    path("api/verify/", views.VerifyCodeAPIView.as_view(), name="verify_code_api"),
+    path("api/login/", views.UserLoginAPIView.as_view(), name="user_login_api"),
    
 ]
