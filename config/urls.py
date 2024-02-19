90b6e3deddefffb2524ba79cf1a743b7e44a0591
MohRezam <mkalhor81126@gmail.com>
2024-02-20 01:15:58 +0330

    Add orders urls

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,7 @@ admin.site.site_title = "DigitBox"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("products.urls", namespace="products")),
+    path("orders/", include("orders.urls", namespace="orders")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
