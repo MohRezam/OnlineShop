3ec9d89ae111e878c1bdd4ee30d1d494ef29b48a
MohRezam <mkalhor81126@gmail.com>
2024-02-21 16:25:57 +0330

    Modify it to sessions again

--- orders/cart.py
+++ orders/cart.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from .serializers import ProductSerializer
 from .models import Product
 import json
-
+from django.http import HttpResponse
 
 CART_SESSION_ID = 'cart'
 
@@ -75,72 +75,73 @@ class Cart:
         self.save()
         
 
-CART_COOKIE_NAME = 'cart'
+# CART_COOKIE_NAME = 'cart'
 
-class Cart:
-    def __init__(self, request):
-        """
-        initialize the cart
-        """
-        self.cookies = request.COOKIES
+# class Cart:
+#     def __init__(self, request):
+#         """
+#         initialize the cart
+#         """
+#         self.cookies = request.COOKIES
         
-        cart_data = self.cookies.get(CART_COOKIE_NAME)
-        if not cart_data:
-            # If no cart data found in cookies, initialize an empty cart
-            cart_data = "{}"
+#         cart_data = self.cookies.get(CART_COOKIE_NAME)
+#         if not cart_data:
+#             # If no cart data found in cookies, initialize an empty cart
+#             cart_data = "{}"
         
-        self.cart = json.loads(cart_data)
+#         self.cart = json.loads(cart_data)
     
-    def save_cart_to_cookies(self, response):
-        """
-        Save the current cart data to cookies
-        """
-        cart_data = json.dumps(self.cart)
-        response.set_cookie(CART_COOKIE_NAME, value=cart_data, max_age=3600)
+#     def save_cart_to_cookies(self, response):
+#         """
+#         Save the current cart data to cookies
+#         """
+#         cart_data = json.dumps(self.cart)
+#         response.set_cookie(CART_COOKIE_NAME, value=cart_data, max_age=3600)
 
-    def add(self, product, quantity=1, overide_quantity=False):
-        """
-        Add product to the cart or update its quantity
-        """
-        product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {
-                "quantity": 0,
-                "price": str(product.price)
-            }
-        self.cart[product_id]["quantity"] += int(quantity)
+#     def add(self, product, quantity=1, overide_quantity=False):
+#         """
+#         Add product to the cart or update its quantity
+#         """
+#         product_id = str(product.id)
+#         if product_id not in self.cart:
+#             self.cart[product_id] = {
+#                 "quantity": 0,
+#                 "price": str(product.price)
+#             }
+#         self.cart[product_id]["quantity"] += int(quantity)
 
-    def remove(self, product):
-        """
-        Remove a product from the cart
-        """
-        product_id = str(product.id)
+#     def remove(self, product, response):
+#         """
+#         Remove a product from the cart
+#         """
+#         product_id = str(product.id)
 
-        if product_id in self.cart:
-            del self.cart[product_id]
+#         if product_id in self.cart:
+#             del self.cart[product_id]
+#             self.save_cart_to_cookies(response)
 
-    def __iter__(self):
-        """
-        Loop through cart items and fetch the products from the database
-        """
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]["product"] = ProductSerializer(product).data
-        for item in cart.values():
-            item["total_price"] = Decimal(item["price"]) * item["quantity"]
-            yield item
+#     def __iter__(self):
+#         """
+#         Loop through cart items and fetch the products from the database
+#         """
+#         product_ids = self.cart.keys()
+#         products = Product.objects.filter(id__in=product_ids)
+#         cart = self.cart.copy()
+#         for product in products:
+#             cart[str(product.id)]["product"] = ProductSerializer(product).data
+#         for item in cart.values():
+#             item["total_price"] = Decimal(item["price"]) * item["quantity"]
+#             yield item
 
-    def __len__(self):
-        """
-        Count all items in the cart
-        """
-        return sum(item["quantity"] for item in self.cart.values())
+#     def __len__(self):
+#         """
+#         Count all items in the cart
+#         """
+#         return sum(item["quantity"] for item in self.cart.values())
 
-    def get_total_price(self):
-        return sum(Decimal(item["price"]) * item["quantity"] for item in self.cart.values())
+#     def get_total_price(self):
+#         return sum(Decimal(item["price"]) * item["quantity"] for item in self.cart.values())
 
-    def clear(self):
-        # Clear the cart by setting it to an empty dictionary
-        self.cart = {}
+#     def clear(self):
+#         # Clear the cart by setting it to an empty dictionary
+#         self.cart = {}
