c5d7d1940e7af55439eb36b71fc0bbdb4ad0f4fb
MohRezam <mkalhor81126@gmail.com>
2024-02-22 01:31:13 +0330

    Modify urls

--- orders/urls.py
+++ orders/urls.py
@@ -4,10 +4,12 @@ from . import views
 app_name = "orders"
 
 urlpatterns = [
+    # path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    # path("detail/<int:order_id>/", views.OrderDetailView.as_view(), name="order_detail"),
     path("cart/", views.CartView.as_view(), name="cart"),
-    path("cart/remove/<slug:product_slug>", views.CartRemoveView.as_view(), name="cart_remove"),
+    path("cart/remove/<slug:product_slug>/", views.CartRemoveView.as_view(), name="cart_remove"),
     
     
     #APIs
-    path("api/cart/<slug:slug>", views.CartAPI.as_view(), name="cart_api"),
+    path("api/cart/<slug:slug>/", views.CartAPI.as_view(), name="cart_api"),
 ]
