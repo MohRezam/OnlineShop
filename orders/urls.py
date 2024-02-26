b26730beea03626bacb762b745c67e9e8cd6a75f
MohRezam <mkalhor81126@gmail.com>
2024-02-27 00:44:50 +0330

    Modify urls

--- orders/urls.py
+++ orders/urls.py
@@ -9,10 +9,14 @@ urlpatterns = [
     path("cart/", views.CartView.as_view(), name="cart"),
     path("cart/remove/<slug:product_slug>/", views.CartRemoveView.as_view(), name="cart_remove"),
     path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    path("detail/<int:order_id>/pay/", views.OrderPayView.as_view(), name="order_pay"),
     path("detail/<int:order_id>/", views.OrderDetailView.as_view(), name="order_detail"),
+    path("verify/", views.OrderVerifyView.as_view(), name="order_verify"),
     
     
     #APIs
     path("api/cart/<slug:slug>/", views.CartAPI.as_view(), name="cart_api"),
-    path("api/order/<int:order_id>/", views.OrderAPIView.as_view(), name="order_api"),
+    # path("api/order/<int:order_id>/", views.OrderAPIView.as_view(), name="order_api"),
+    path("detail/<int:order_id>/api/", views.OrderDetailAPIView.as_view(), name="order_detail_api"),
+    
 ]
--- orders/views.py
+++ orders/views.py
@@ -14,6 +14,9 @@ from .serializers import OrderSerializer, ProductSerializer, CouponSerializer
 from django.contrib import messages
 from rest_framework.renderers import JSONRenderer
 from rest_framework.serializers import ValidationError
+from django.conf import settings
+import requests
+import json
 
 class CartView(View):   
     def get(self, request):
@@ -96,11 +99,16 @@ class CartRemoveView(APIView):
         return response
         
    
-class OrderDetailView(LoginRequiredMixin, View):
+class OrderDetailAPIView(APIView):
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
-        return render(request, "orders/checkout.html")
+        serializer = OrderSerializer(order)
+        return Response({'serializer':serializer.data})
            
+           
+class OrderDetailView(View):
+    def get(self, request, order_id):
+        return render(request, 'orders/checkout.html')
      
 # class OrderCreateView(LoginRequiredMixin, View):
 #     def get(self, request):
@@ -133,20 +141,89 @@ class OrderCreateView(LoginRequiredMixin, View):
             quantity = item["quantity"]
             product = Product.objects.get(id=product_id)
             OrderItem.objects.create(order=order, product= product, quantity=quantity)
-        return render(request, 'orders/checkout.html')  
+        return redirect('orders:order_detail', order.id)  
     
-class OrderAPIView(APIView):
-    # permission_classes = [IsAuthenticated]
-    def get(self, request, order_id):
-        order = get_object_or_404(Order, pk=order_id)
-        # coupon = Coupon(order=order)
-        # coupon_ser = CouponSerializer(instance=coupon)
-        order_ser = OrderSerializer(order)
+# class OrderAPIView(APIView):
+#     # permission_classes = [IsAuthenticated]
+#     def get(self, request, order_id):
+#         order = get_object_or_404(Order, pk=order_id)
+#         # coupon = Coupon(order=order)
+#         # coupon_ser = CouponSerializer(instance=coupon)
+#         order_ser = OrderSerializer(order)
         
-        responses_data = {
-            # "coupon":coupon_ser.data,
-            "order":order_ser.data,
+#         responses_data = {
+#             # "coupon":coupon_ser.data,
+#             "order":order_ser.data,
             
-        }
-        return Response(data=responses_data, status=status.HTTP_200_OK)
+#         }
+#         return Response(data=responses_data, status=status.HTTP_200_OK)
     
+    
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+CallbackURL = 'http://127.0.0.1:8080/orders/verify/'
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        request.session['order_pay'] = {
+            'order_id': order.id,
+        }
+        data = {
+            "MerchantID": settings.MERCHANT,
+            "Amount": str(order.calculate_total_price()),  # Convert Decimal to string
+            "Description": description,
+            "Phone": request.user.phone_number,
+            "CallbackURL": CallbackURL,
+        }
+        data = json.dumps(data)
+        # set content length by data
+        headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+        try:
+            response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                response_data = response.json()
+                if response_data['Status'] == 100:
+                    return JsonResponse({'status': True, 'url': ZP_API_STARTPAY + str(response_data['Authority']), 'authority': response_data['Authority']})
+                else:
+                    return JsonResponse({'status': False, 'code': str(response_data['Status'])})
+            return JsonResponse(response_data)
+
+        except requests.exceptions.Timeout:
+            return JsonResponse({'status': False, 'code': 'timeout'})
+        except requests.exceptions.ConnectionError:
+            return JsonResponse({'status': False, 'code': 'connection error'})
+
+
+class OrderVerifyView(LoginRequiredMixin, View):
+    def get(self, request):
+        order_id = request.session["order_pay"]["order_id"]
+        order = Order.objects.get(id=int(order_id))
+        data = {
+        "MerchantID": settings.MERCHANT,
+        "Amount": order.calculate_total_price(),
+        "Authority": request.GET['Authority'],
+    }
+        data = json.dumps(data)
+        # set content length by data
+        headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
+        response = requests.post(ZP_API_VERIFY, data=data,headers=headers)
+
+        if response.status_code == 200:
+            order.is_paid = True
+            order.save()
+            response = response.json()
+            if response['Status'] == 100:
+                return {'status': True, 'RefID': response['RefID']}
+            else:
+                return {'status': False, 'code': str(response['Status'])}
+        return response
