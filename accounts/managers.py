32d7a4aadcdaaa7ebdcbc1f660f2381f82749422
MohRezam <mkalhor81126@gmail.com>
2024-03-12 21:59:23 +0330

    Modify the createsuper user method and set is_staff and is_superuser to True when creating superuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -74,6 +74,8 @@ class UserManager(BaseUserManager):
             password=password,
         )
         user.is_staff = True
+        user.role = "admin"
+        user.is_superuser = True
         user.save(using=self._db)
         return user
     
