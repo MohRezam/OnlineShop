dea9112516bc80b0cb6add2df6a931a10e33ae31
MohRezam <mkalhor81126@gmail.com>
2024-03-13 22:04:36 +0330

    Modify CommentAPI and add post method for adding new comments

--- products/views.py
+++ products/views.py
@@ -179,6 +179,14 @@ class CommentAPIView(APIView):
             return Response({'comment': serializer.data}, status=status.HTTP_200_OK)
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    def post(self, request, slug):
+        comment = CommentSerializer(data=request.data)
+        product = Product.objects.get(slug=slug)
+        if comment.is_valid():
+            Comment.objects.create(text=comment.validated_data["text"], user=request.user, product=product)
+            return Response(status=status.HTTP_200_OK)
+        print(comment.errors)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentView(View):
