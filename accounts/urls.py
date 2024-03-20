2d103438f66f34e4de30d2c256bfd74b99a4d0d2
MohRezam <mkalhor81126@gmail.com>
2024-03-20 09:11:41 +0330

    Modify register api and verify api urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('order_history/' , views.OrderHistory.as_view() , name="order_history"),
     
     #API
-    path("api/register/", views.UserRegisterAPIView.as_view(), name="user_register_api"),
-    path("api/verify/", views.VerifyCodeAPIView.as_view(), name="verify_code_api"),
+    path("register/api/v1/", views.UserRegisterAPIView.as_view(), name="user_register_api"),
+    path("verify/api/v1/", views.VerifyCodeAPIView.as_view(), name="verify_code_api"),
     # path("login/api/", views.LoginView.as_view(), name="user_login_api"),
     path("panel/api/v1/", views.CustomerPanelAPIView.as_view(), name="user_panel_api"),
     path('address/edit/<int:address_id>/api/v1/', views.EditAddressAPIView.as_view(), name='edit_address_api_v1'),
