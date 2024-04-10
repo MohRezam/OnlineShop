745a76ea23cded4d44817e013a1da5ecd59c4018
MohRezam <mkalhor81126@gmail.com>
2024-04-10 13:27:16 +0330

    Modify the search

--- static/scripts/products.js
+++ static/scripts/products.js
@@ -2,38 +2,48 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentPage = 1;
     let totalPages = 1;
 
-    function fetchProducts(page) {
-        fetch('/api' + window.location.pathname + `?page=${page}`)
+    function fetchProducts(page, searchQuery = '') {
+        let url = '/api' + window.location.pathname + `?page=${page}`;
+
+        if (searchQuery) {
+            url = `/api/product/search/?search=${searchQuery}`;
+        }
+
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 const productsRow = document.getElementById('productsRow');
                 productsRow.innerHTML = '';
 
-                data.results.forEach(product => {
-                    const colDiv = document.createElement('div');
-                    colDiv.classList.add('col-md-4', 'margin_bottom1');
+                if (data.results) {
+                    data.results.forEach(product => {
+                        const colDiv = document.createElement('div');
+                        colDiv.classList.add('col-md-4', 'margin_bottom1');
 
-                    const productBox = document.createElement('div');
-                    productBox.classList.add('product_box');
+                        const productBox = document.createElement('div');
+                        productBox.classList.add('product_box');
 
-                    const figure = document.createElement('figure');
-                    const img = document.createElement('img');
-                    img.src = product.image;
-                    img.alt = product.name;
-                    figure.appendChild(img);
+                        const figure = document.createElement('figure');
+                        const img = document.createElement('img');
+                        img.src = product.image;
+                        img.alt = product.name;
+                        figure.appendChild(img);
 
-                    const productNameLink = document.createElement('a');
-                    productNameLink.textContent = product.name;
-                    productNameLink.href = `/products/${product.slug}`;
-                    productNameLink.classList.add('product_name');
+                        const productNameLink = document.createElement('a');
+                        productNameLink.textContent = product.name;
+                        productNameLink.href = `/products/${product.slug}`;
+                        productNameLink.classList.add('product_name');
 
-                    productBox.appendChild(figure);
-                    productBox.appendChild(productNameLink);
+                        productBox.appendChild(figure);
+                        productBox.appendChild(productNameLink);
 
-                    colDiv.appendChild(productBox);
-                    productsRow.appendChild(colDiv);
-                });
-
+                        colDiv.appendChild(productBox);
+                        productsRow.appendChild(colDiv);
+                    });
+                }
+                else {
+                    console.error('No results found');
+                }
                 currentPage = page;
                 totalPages = Math.ceil(data.count / data.results.length);
 
@@ -60,57 +70,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function searchProducts(query) {
-        fetch(`/api/product/search/?search=${query}`)
-            .then(response => response.json())
-            .then(data => {
-                const productsRow = document.getElementById('productsRow');
-                productsRow.innerHTML = '';
-
-                data.forEach(product => {
-                    const colDiv = document.createElement('div');
-                    colDiv.classList.add('col-md-4', 'margin_bottom1');
-
-                    const productBox = document.createElement('div');
-                    productBox.classList.add('product_box');
-
-                    const figure = document.createElement('figure');
-                    const img = document.createElement('img');
-                    img.src = product.image;
-                    img.alt = product.name;
-                    figure.appendChild(img);
-
-                    const productNameLink = document.createElement('a');
-                    productNameLink.textContent = product.name;
-                    productNameLink.href = `/products/${product.slug}`;
-                    productNameLink.classList.add('product_name');
-
-                    productBox.appendChild(figure);
-                    productBox.appendChild(productNameLink);
-
-                    colDiv.appendChild(productBox);
-                    productsRow.appendChild(colDiv);
-                });
-
-                currentPage = 1;
-                totalPages = Math.ceil(data.count / data.results.length);
-                updatePaginationButtons();
-            })
-            .catch(err => {
-                console.error('Error fetching products data:', err);
-            });
-    }
-
     const searchForm = document.querySelector('#searchForm');
     if (searchForm) {
         searchForm.addEventListener('submit', function (event) {
             event.preventDefault();
             const searchInput = document.getElementById('gsearch');
             const query = searchInput.value.trim();
-            searchProducts(query);
+            if (query) {
+                fetchProducts(1, query);
+            }
         });
     }
 
     fetchProducts(currentPage);
-    searchProducts('');
 });
