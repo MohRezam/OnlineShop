69e9befe627de3bf08c9bdd67f34c28e16d9881a
MohRezam <mkalhor81126@gmail.com>
2024-04-12 02:37:08 +0330

    Modify the email address

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -5,6 +5,6 @@ from django.core.mail import send_mail
 def send_otp_email(email, otp):
     subject = 'Your OTP Code'
     message = f'Your OTP code is: {otp}'
-    from_email = 'shadmehrihesam@gmail.com'
+    from_email = 'mkalhor81126@gmail.com'
     recipient_list = [email]
     send_mail(subject, message, from_email, recipient_list)
